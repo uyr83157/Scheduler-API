20b4969cd67dd1da01ca3571199466d8001dc545
uyr83157 <uyr83157@gmail.com>
2025-01-28 23:14:22 +0900

    feat: User, Schedule, ImportantSchedule entity

--- src/main/java/com/example/scheduler/entity/ImportantSchedule.java
+++ src/main/java/com/example/scheduler/entity/ImportantSchedule.java
@@ -1,4 +1,19 @@
 package com.example.scheduler.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ImportantSchedule {
+
+    private Long id;
+    private User user;
+    private Schedule schedule;
+    private Long createdBy;
+    private LocalDateTime importantRegisteredAt;
+    private String importantRegisteredBy;
+
 }
--- src/main/java/com/example/scheduler/entity/User.java
+++ src/main/java/com/example/scheduler/entity/User.java
@@ -1,4 +1,22 @@
 package com.example.scheduler.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String userPassword;
+    private LocalDateTime signUpDate;
+    private int  scheduleCount;
+    private String userStatus;
+    private Byte userLevel;
+
+
 }
