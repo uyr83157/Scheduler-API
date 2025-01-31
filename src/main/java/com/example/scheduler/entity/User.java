17d804b953927094f59eb2e42f2f9f5fa0a01411
uyr83157 <uyr83157@gmail.com>
2025-01-31 23:45:02 +0900

    feat: Add DB(MySQL), Delete ImportantSchedule Entity

--- src/main/java/com/example/scheduler/entity/User.java
+++ src/main/java/com/example/scheduler/entity/User.java
@@ -1,22 +1,25 @@
 package com.example.scheduler.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Getter // lombok 자동 Getter 생성
+@Setter // lombok 자동 Setter 생성
+@NoArgsConstructor // lombok 자동 필드 생성자 생성
+@AllArgsConstructor // lombok 자동 기본 생성자 생성
 public class User {
 
-    private Long id;
-    private String name;
-    private String email;
-    private String userPassword;
-    private LocalDateTime signUpDate;
-    private int  scheduleCount;
-    private String userStatus;
-    private Byte userLevel;
-
+    private Long userId; // 유저 식별자
+    private String name; // 유저 이름
+    private String email; // 유저 이메일
+    private String userPassword; // 유저 패스워드
+    private LocalDateTime signUpDate; // 유저 가입 날짜+시간
+    private int  scheduleCount; // 총 작성한 일정 수
+    private String userStatus; // 유저 제재 상태
+    private Byte userLevel; // 유저 권한 레벨
 
 }
