71645ccbd1a889206b691f844d30992d77761a22
uyr83157 <uyr83157@gmail.com>
2025-02-03 01:34:26 +0900

    fix: 전역 변수 수정

--- src/main/java/com/example/scheduler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/scheduler/exception/GlobalExceptionHandler.java
@@ -37,10 +37,4 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
-    // 기타 모든 예외 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-    }
-
 }
